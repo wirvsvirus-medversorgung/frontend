9e1f51b707fcdd9f940ac1d31109954a388e1df8
schokotets <moritzleni@gmail.com>
2020-03-21 22:33:01 +0100

    create content (motivation)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,4 +9,12 @@
 </svelte:head>
 
 <p>Wir vermitteln Studierende zu Krankenhäusern, die Unterstützung benötigen.</p>
-<a href="/motivation">Unsere Motivation</a>
+<a href="/motivation"><p>Unsere Motivation</p></a>
+
+<h2>Mitmachen!</h2>
+
+<a href="/signup-student"><p>Als Medizinstudent:in aushelfen</p></a>
+
+<a href="/signup-hostpital"><p>Als Krankenhaus anmelden</p></a>
+
+<p><i>Entstanden im Rahmen des Hackathons #WirVsVirus</i></p>
---
+++ src/routes/motivation.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+	<title>Motivation</title>
+</svelte:head>
+
+<h1>Unsere Motivation</h1>
+
+<p>COVID-19 führt global zu einer starken Belastung der Menschen und Ressourcen des Gesundheitssystem.
+Es zeichnet sich ab, dass auch Personal im deutschen Gesundheitswesen eine immer knapper werdende Ressource sein wird.</p>
+<p>Viele Medizinstudierende sind sehr engagiert und voller Tatkraft, um Krankenhäuser, Ambulanzen der kassenärztlichen
+Vereinigungen und niedergelassene Praxen, zu unterstützen. Durch den verschobenen Semesterbeginn sind viele Medizinstudierende zeitlich flexibel.</p>
+<p>Mit MedSupport möchten wir helfen, die zahlreichen Medizinstudierenden mit allen Institutionen zu verbinden, damit Unterstützung dort ankommt, wo sie am meisten benötigt wird. 
+#wirvsvirus</p>
