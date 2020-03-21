a605ff5c0ebfda9f313c77a49a60a2a2e9a45d2c
schokotets <moritzleni@gmail.com>
2020-03-21 23:49:20 +0100

    refine student signup form

--- src/routes/signup-student.svelte
+++ src/routes/signup-student.svelte
@@ -3,33 +3,40 @@
 
 <form>
     <label>Vorname</label>
-    <input type="text" name="vorname">
+    <input type="text" name="vorname" required>
     <br/>
     <label>Nachname</label>
-    <input type="text" name="nachname">
+    <input type="text" name="nachname" required>
     <br/>
     <label>Telefonnummer</label>
-    <input type="text" name="phonenumber">
+    <input type="text" name="phonenumber" required>
+    <br/>
+    <label>Ich studiere Medizin im sovielten Semester</label>
+    <input type="number" min=1 name="semester">
     <br/>
     <label>E-Mail-Adresse</label>
-    <input type="text" name="emailaddress">
+    <input type="text" name="emailaddress" required>
     <br/>
     <label>Passwort</label>
-    <input type="password" name="password">
+    <input type="password" name="password" required>
     <br/>
     <label>Passwort bestätigen</label>
-    <input type="password" name="password-confirmation">
+    <input type="password" name="password-confirmation" required>
+    <br/>
+    <input type="checkbox" name="confirm-dataprocessing" required>
+    <label>Ich stimme zu, dass meine persönlichen Angaben gemäß der <a href="/datenschutz">Datenschutzerklärung</a> aufgenommen und verarbeitet werden.</label>
     <br/>
     <input type="submit" value="Registieren">
 </form>
 
 <style>
-    br {
-        display: block;
-        height: 2em;
+     input[type=number], input[type=text], input[type=password] {
+        margin-bottom: 1rem;
     }
-    label, input[type=submit] {
+    input[type=submit] {
+        margin-top: 1rem;
+    }
+    br + label, label:first-child {
         display: block;
-        margin-top: 1em;
     }
 </style>
