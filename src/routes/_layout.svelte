ecd601f3790a12310d4d166e10f712ff2b025618
schokotets <moritzleni@gmail.com>
2020-03-21 22:20:21 +0100

    cleanup styles

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,18 +2,15 @@
 	import Header from '../components/Header.svelte';
 </script>
 
-<link href="https://fonts.googleapis.com/css2?family=Karla&family=Tinos:wght@700&display=swap" rel="stylesheet"/>
-
 <Header/>
 <main>
 	<slot></slot>
 </main>
 
 <style>
-	main {
+	:global(main) {
 		position: relative;
 		max-width: 56em;
-		background-color: white;
 		padding: 2em;
 		margin: 0 auto;
 		box-sizing: border-box;
