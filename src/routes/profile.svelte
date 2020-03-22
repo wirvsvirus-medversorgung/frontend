69ff447d233d5eeea7ce9414cd74660d9614d5d5
schokotets <moritzleni@gmail.com>
2020-03-22 16:40:39 +0100

    add profile view

---
+++ src/routes/profile.svelte
@@ -0,0 +1,8 @@
+<script>
+import LoggedIn, {currentUser} from '../components/LoggedIn.svelte'
+</script>
+
+<h2>Dein Profil</h2>
+
+<p><strong>Name:</strong> {$currentUser.name}</p>
+<p><strong>EMail:</strong> {$currentUser.email}</p>
