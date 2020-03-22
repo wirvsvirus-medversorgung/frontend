8a64223d6ff17bf85602db53ee245670606826e8
schokotets <moritzleni@gmail.com>
2020-03-22 22:41:33 +0100

    fix typo in motivation

--- src/routes/motivation.svelte
+++ src/routes/motivation.svelte
@@ -4,7 +4,7 @@
 
 <h2>Unsere Motivation</h2>
 
-<p>COVID-19 führt global zu einer starken Belastung der Menschen und Ressourcen des Gesundheitssystem.
+<p>COVID-19 führt global zu einer starken Belastung der Menschen und Ressourcen des Gesundheitssystems.
 Es zeichnet sich ab, dass auch Personal im deutschen Gesundheitswesen eine immer knapper werdende Ressource sein wird.</p>
 <p>Viele Medizinstudierende sind sehr engagiert und voller Tatkraft, um Krankenhäuser, Ambulanzen der kassenärztlichen
 Vereinigungen und niedergelassene Praxen, zu unterstützen. Durch den verschobenen Semesterbeginn sind viele Medizinstudierende zeitlich flexibel.</p>
