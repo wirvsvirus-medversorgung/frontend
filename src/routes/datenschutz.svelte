c3cc13ca6be7bdd79921fb49db75e31539b61016
schokotets <moritzleni@gmail.com>
2020-03-22 13:19:46 +0100

    datenschutz

---
+++ src/routes/datenschutz.svelte
@@ -0,0 +1,18 @@
+<svelte:head>
+	<title>Datenschutz</title>
+</svelte:head>
+
+<h1>Datenschutz</h1>
+
+<p>Wir nehmen persönliche Kontaktdaten auf, um den Kontakt zwischen Studenten und Krankenhäusern herzustellen.</p>
+<p>Der Wohnort des Studierenden dient zum Matching nahegelegener bedürftiger Krankenhäuser.</p>
+<p>Es werden keine Daten an Dritte weitergegeben.</p>
+
+<!-- TODO do not loose form data -->
+<a on:click|once={goBack}>zurück</a>
+
+<script>
+function goBack() {
+  window.history.back();
+}
+</script>
